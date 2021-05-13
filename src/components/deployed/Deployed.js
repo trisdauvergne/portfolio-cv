dff7d10ebd6293001cfa5e048b5a095678388001
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-05-13 14:04:44 +0200

    Added anchor tags to deployed links

---
+++ src/components/deployed/Deployed.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+
+const Deployed = () => {
+  // useEffect(() => {
+  //   window.location.href = 
+  // });
+
+  return (
+    <div>
+      <h1>Deployed pages</h1>
+    </div>
+  )
+}
+
+export default Deployed
