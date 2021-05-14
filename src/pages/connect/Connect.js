0462dae81d326d1da595dd8d3664a9eacf0f40b5
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-05-14 13:46:18 +0200

    Added plaeholders to the connect section

--- src/pages/connect/Connect.js
+++ src/pages/connect/Connect.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './connect.css';
 
 const Connect = () => {
+  const [visibleConnections, setVisibleConnections] = useState(false);
   return (
-    <section className="content-section">
-      <h1 className="heading">Connect</h1>
+    <section className="connect content-section">
+      <div className="heading-div">
+        <h1 onClick={() => setVisibleConnections(!visibleConnections)} className="heading">Connect</h1>
+      </div>
+      {visibleConnections && <div className="connections">
+        <h3>LinkedIn</h3>
+        <h3>GitHub</h3>
+        <h3>Email</h3></div>}
     </section>
   )
 }
