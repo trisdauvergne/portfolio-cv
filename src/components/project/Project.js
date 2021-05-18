355fc171a481f9f6778e1f0cef47d84ecfaa054b
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-05-18 16:33:20 +0200

    Tweaks made to CSS

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 import './project.css';
 
-
 const Project = ({ project }) => {
   const [toolsAndLanguagesVisible, setToolsAndLanguagesVisible] = useState(false);
   const [projectBodyVisible, setProjectBodyVisible] = useState(false);
-  // console.log('in project.js', project);
+  console.log('in project.js', project);
 
   return (
     <section className="project-section">
@@ -13,15 +12,16 @@ const Project = ({ project }) => {
         <h3 className="project-heading">{project.projectTitle}</h3>
         <div className="txt--centred project-content">
           <p>{project.projectIntro}</p>
-          <h5 className="custom-cursor" onClick={() => setProjectBodyVisible(!projectBodyVisible)} onMouseOut={() => setProjectBodyVisible(false)}>READ MORE +</h5>
-          {projectBodyVisible && <div>
+          {/* <img className="project-preview" src={project.mainPagePreview.file.url}/> */}
+          <h5 className="custom-cursor" onClick={() => setProjectBodyVisible(!projectBodyVisible)}>{projectBodyVisible ? 'HIDE -':'READ MORE +'}</h5>
+          {projectBodyVisible && <div onMouseLeave={() => setProjectBodyVisible(false)}>
             <p>{project.projectBody.projectBody}</p>
             <p>{project.projectCreative.projectCreative}</p>
           </div>}
-          <h5 className="custom-cursor" onMouseOut={() => setToolsAndLanguagesVisible(false)} onClick={() => setToolsAndLanguagesVisible(!toolsAndLanguagesVisible)}>TOOLS AND LANGUAGES +</h5>
-          {toolsAndLanguagesVisible && <>
+          <h5 className="custom-cursor" onClick={() => setToolsAndLanguagesVisible(!toolsAndLanguagesVisible)}>{toolsAndLanguagesVisible ? 'HIDE -' : 'TOOLS & LANGUAGES +'}</h5>
+          {toolsAndLanguagesVisible && <div onMouseLeave={() => setToolsAndLanguagesVisible(false)} >
             <p>{project.toolsAndLanguages.join(', ')}</p>
-          </>}
+          </div>}
           <div className="project-btns">
             <button className="button project-btn deploy-btn"><a href={project.deployedLink} target="_blank" rel="noreferrer">Deployed project<span class="material-icons small-icon">arrow_right_alt</span></a></button>
             <button className="button project-btn source-btn"><a href={project.sourceCode} target="_blank" rel="noreferrer">Source code<span class="material-icons small-icon">arrow_right_alt</span></a></button>
