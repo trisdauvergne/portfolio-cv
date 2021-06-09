30bf798e7e7dfebd878452a96af6caa8f751dc34
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-06-09 10:10:09 +0200

    Updated the about section

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -6,7 +6,13 @@ const About = () => {
   return (
     <section id="about" className="section about-section">
       <h3 className="heading about__heading">About me.</h3>
-      <p className="txt--centred about__txt">A former design project manager from London turned developer, living in Stockholm. I decided to pursue a career as a developer to help organisations create meaningful online experiences for their customers. A lover of food, music and good design.</p>
+      <p className="txt--centred about__txt">A former design project manager from London now turned developer and living in Stockholm.</p>
+      <br/>
+      <p className="txt--centred about__txt">Passionate about UX, UI (and good design in general), music and food.</p>
+      <br/>
+      <p className="txt--centred about__txt">I decided to pursue a career as a developer after seeing the exciting online experiences many brands were creating in response to changing customer behaviours.</p>
+      <br/>
+      <p className="txt--centred about__txt">Being a developer allows me to create meaningful online experiences that respond to problems with creative solutions.</p>
     </section>
   )
 }
